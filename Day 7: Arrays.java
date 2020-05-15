6480a4c1a5dbd24a705696666297361e1578512b
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-15 12:57:00 +0530

    Hackerrank solution

--- Day 7: Arrays.java
+++ Day 7: Arrays.java
@@ -7,6 +7,7 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
+        
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int[] arr = new int[n];
