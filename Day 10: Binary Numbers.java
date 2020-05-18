1144af2f57c04be165c549addabd9ad57c3a8b47
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-18 22:44:35 +0530

    Hackerrank solution

--- Day 10: Binary Numbers.java
+++ Day 10: Binary Numbers.java
@@ -6,3 +6,31 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
+public class Solution {
+
+public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    int rem=0,s=0,t=0;
+
+
+    while(n>0)
+        {
+        rem=n%2;
+        n=n/2;
+        if(rem==1)
+         {   s++;
+           if(s>=t)
+
+            t=s;
+
+        }
+        else{
+
+            s=0;
+        }   
+    }
+
+    System.out.println(t);
+}
+}
