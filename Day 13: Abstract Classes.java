18eb18243a9cbe065fcf2056614e35379a78a63b
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-21 23:05:06 +0530

    Hackerrank solution

--- Day 13: Abstract Classes.java
+++ Day 13: Abstract Classes.java
@@ -25,3 +25,17 @@ class MyBook extends Book {
         System.out.println("Price: " + this.price);
     }
 }
+
+public class Solution {
+   
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        String author = scanner.nextLine();
+        int price = scanner.nextInt();
+        scanner.close();
+
+        Book book = new MyBook(title, author, price);
+        book.display();
+    }
+}
