e76323e07c674002ec6b3685a8ee63c751d23e92
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-21 23:04:50 +0530

    Update Day 13: Abstract Classes.java

--- Day 13: Abstract Classes.java
+++ Day 13: Abstract Classes.java
@@ -11,3 +11,17 @@ abstract class Book {
     
     abstract void display();
 }
+
+class MyBook extends Book {
+    private int price;
+    public MyBook (String title, String author, int price) {
+        super(title, author);
+        this.price = price;
+    }
+    
+    void display() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + this.price);
+    }
+}
