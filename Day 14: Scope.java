a8aca4bd792b8eca1860c39ecabd0a71799ce333
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-22 22:25:21 +0530

    Update Day 14: Scope.java

--- Day 14: Scope.java
+++ Day 14: Scope.java
@@ -12,3 +12,15 @@ class Difference {
     public Difference(int[] a) {
   elements = a;
 }
+
+  public void computeDifference() {
+  int max = 0;
+  for (int i = 0; i < elements.length-1; i++) {
+      for (int j = i+1; j < elements.length; j++) {
+          int diff = Math.abs(elements[i] - elements[j]);
+          if (diff > max) 
+              max = diff;
+      }
+  }
+  maximumDifference = max;
+}
