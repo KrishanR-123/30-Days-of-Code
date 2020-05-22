00f31cfa131ee709dc371e017922d941a8dd1d10
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-22 22:26:56 +0530

    Hackerrank solution

--- Day 14: Scope.java
+++ Day 14: Scope.java
@@ -24,3 +24,23 @@ class Difference {
   }
   maximumDifference = max;
 }
+  
+// End of Difference class
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
+    }
+}  
