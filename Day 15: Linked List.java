e792c8df29ba2b7cd3fac4900cc9f080ce31e4d0
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-25 23:07:32 +0530

    Hackerrank Solution

--- Day 15: Linked List.java
+++ Day 15: Linked List.java
@@ -47,3 +47,16 @@ public static  Node insert(Node head,int data) {
         }
     }
 
+public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        int N = sc.nextInt();
+
+        while(N-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head,ele);
+        }
+        display(head);
+        sc.close();
+    }
+}
