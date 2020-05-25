3c15958b2aa773343c8f07cd9121c918ddf280a9
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-25 23:12:08 +0530

    Update Day 17: More Exceptions.java

--- Day 17: More Exceptions.java
+++ Day 17: More Exceptions.java
@@ -1,3 +1,21 @@
 import java.util.*;
 import java.io.*;
 
+//Write your code here
+class Calculator
+{
+    
+    public int power(int n,int p) throws Exception
+    {
+        if(n>=0 && p >=0)
+        {
+            return (int)Math.pow(n,p);
+        }
+        else
+        {
+            throw new Exception("n and p should be non-negative");
+        }
+   
+    }
+    
+}
