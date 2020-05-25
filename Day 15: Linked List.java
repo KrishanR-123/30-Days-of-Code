f3ab6e0a941c822642aefa0f65e66f8b44c0854b
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-25 23:06:33 +0530

    Update Day 15: Linked List.java

--- Day 15: Linked List.java
+++ Day 15: Linked List.java
@@ -9,3 +9,33 @@ class Node {
         next = null;
     }
 }
+
+class Solution {
+public static  Node insert(Node head,int data) {
+      // This is the method which we have to add.
+      Node current=head;
+       Node temp=null;
+        if(current==null)
+        {
+            temp=new Node(data);
+            current=temp;
+            return temp;
+        }
+        
+        else 
+        {
+            Node temp1=current;
+          while(current.next!=null)
+          {
+              current=current.next;
+          }
+            current.next= new Node(data);
+            current= current.next;
+            current.data =data;
+            
+            return temp1;
+
+        }   
+        
+}
+
