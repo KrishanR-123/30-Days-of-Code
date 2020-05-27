294dd4f53d8b1dba63173aab13ed10f676098e54
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-27 22:32:29 +0530

    Update Day 18: Queues and Stacks.java

--- Day 18: Queues and Stacks.java
+++ Day 18: Queues and Stacks.java
@@ -1,3 +1,7 @@
 import java.io.*;
 import java.util.*;
 
+public class Solution {
+    // Write your code here.
+    Stack<Character> stack = new Stack<Character>();
+    Queue<Character> queue = new LinkedList<Character>();
