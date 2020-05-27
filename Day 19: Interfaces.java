a655cc7358bfcb253c8bbd9dc72ef685c5d7b50a
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-27 22:37:09 +0530

    Update Day 19: Interfaces.java

--- Day 19: Interfaces.java
+++ Day 19: Interfaces.java
@@ -4,3 +4,14 @@ import java.util.*;
 interface AdvancedArithmetic{
    int divisorSum(int n);
 }
+
+class Calculator implements AdvancedArithmetic {
+    public int divisorSum(int n) {
+        int sum = n;
+        for (int i = 1; i <= n/2; i++) {
+            if (n % i == 0)
+                sum += i;
+        }
+        return sum;
+    }
+}
