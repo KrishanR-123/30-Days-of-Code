4d89ea71ebdc3e1be74903b6393b605d21d443d3
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-27 22:37:31 +0530

    Hackerrank solution

--- Day 19: Interfaces.java
+++ Day 19: Interfaces.java
@@ -15,3 +15,17 @@ class Calculator implements AdvancedArithmetic {
         return sum;
     }
 }
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        
+      	AdvancedArithmetic myCalculator = new Calculator(); 
+        int sum = myCalculator.divisorSum(n);
+        System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName() );
+        System.out.println(sum);
+    }
+}
