0d696e12aa435556a106b63e48252eb4787cf7f5
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-30 19:54:49 +0530

    Hackerrank solution

--- Day 22: Binary Search Trees.java
+++ Day 22: Binary Search Trees.java
@@ -49,3 +49,15 @@ class Solution{
         }
     }
 
+public static void main(String args[]){
+            Scanner sc=new Scanner(System.in);
+            int T=sc.nextInt();
+            Node root=null;
+            while(T-->0){
+                int data=sc.nextInt();
+                root=insert(root,data);
+            }
+            int height=getHeight(root);
+            System.out.println(height);
+        }	
+}
