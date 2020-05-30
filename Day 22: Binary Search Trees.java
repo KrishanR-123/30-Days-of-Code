fa882c2fa262bdfc14e514a7f7a22167ed5aba4a
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-30 19:54:25 +0530

    Update Day 22: Binary Search Trees.java

--- Day 22: Binary Search Trees.java
+++ Day 22: Binary Search Trees.java
@@ -31,4 +31,21 @@ class Solution{
             return (rHeight+1);
       }
     }
+	public static Node insert(Node root,int data){
+        if(root==null){
+            return new Node(data);
+        }
+        else{
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
 
