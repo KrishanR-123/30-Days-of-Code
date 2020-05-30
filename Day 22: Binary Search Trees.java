87934067921a382e5e5f87aa4cd688d47814d2bf
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-30 19:54:07 +0530

    Update Day 22: Binary Search Trees.java

--- Day 22: Binary Search Trees.java
+++ Day 22: Binary Search Trees.java
@@ -9,3 +9,26 @@ class Node{
         left=right=null;
     }
 }
+class Solution{
+
+	public static int getHeight(Node root){
+      //Write your code here
+      
+       if(root == null)
+      {
+          return -1;
+      }
+      else
+      {
+        int lHeight= getHeight(root.left);
+        int rHeight=getHeight(root.right);
+ 
+        if(lHeight>rHeight)
+        {
+            return (lHeight+1);
+        }
+        else
+            return (rHeight+1);
+      }
+    }
+
