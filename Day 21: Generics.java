a35dab96318ba2a91b2b345447a4df6639178418
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-30 19:49:41 +0530

    Update Day 21: Generics.java

--- Day 21: Generics.java
+++ Day 21: Generics.java
@@ -1,3 +1,21 @@
 import java.util.*;
 
 class Printer <T> {
+
+  /**
+    *    Method Name: printArray
+    *    Print each element of the generic array on a new line. Do not return anything.
+    *    @param A generic array
+    **/
+    
+    // Write your code here
+ public void printArray(T[] array)
+    {
+        for(T value:array)
+        {
+            System.out.println(value);
+        }
+ 
+    }
+  
+  
