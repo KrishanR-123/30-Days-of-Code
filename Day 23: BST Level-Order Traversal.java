8246a0e8650db6b76ddd84416d861070b2a7c8e5
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-31 23:10:48 +0530

    Update Day 23: BST Level-Order Traversal.java

--- Day 23: BST Level-Order Traversal.java
+++ Day 23: BST Level-Order Traversal.java
@@ -26,3 +26,20 @@ class Solution{
               queue.add(node.right);
       }
   }
+	public static Node insert(Node root,int data){
+        if(root==null){
+            return new Node(data);
+        }
+        else{
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
