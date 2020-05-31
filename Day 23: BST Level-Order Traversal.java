591c20f06d9b0ac68e040501be554e87e2b2fd8c
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-05-31 23:11:12 +0530

    Hackerrank solution

--- Day 23: BST Level-Order Traversal.java
+++ Day 23: BST Level-Order Traversal.java
@@ -43,3 +43,14 @@ class Solution{
             return root;
         }
     }
+public static void main(String args[]){
+            Scanner sc=new Scanner(System.in);
+            int T=sc.nextInt();
+            Node root=null;
+            while(T-->0){
+                int data=sc.nextInt();
+                root=insert(root,data);
+            }
+            levelOrder(root);
+        }	
+}
