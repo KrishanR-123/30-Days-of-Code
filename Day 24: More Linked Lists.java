cdbde1e2b0e518473dd06cd6e4d443477fb90c7a
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-06-01 22:52:34 +0530

    Update Day 24: More Linked Lists.java

--- Day 24: More Linked Lists.java
+++ Day 24: More Linked Lists.java
@@ -10,3 +10,31 @@ class Node{
     }
 	
 }
+
+class Solution
+{
+
+    public static Node removeDuplicates(Node head) {
+     //Write your code here
+       Node currentNode = head;
+       while(currentNode!=null && currentNode.next!=null)
+           {
+           Node node = currentNode;
+           while(node.next!=null)
+               {
+               if(node.next.data==currentNode.data)
+                   {
+                   Node next = node.next.next;
+                   Node temp= node.next;
+                   node.next=next;
+                   temp=null;
+
+               }
+               else{
+               node=node.next;
+               }
+           }
+           currentNode=currentNode.next;
+       }
+       return head;
+   }
