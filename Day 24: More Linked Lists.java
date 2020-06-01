cba73b995e354bffa90a86ae04b151c704d35ac3
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-06-01 22:53:11 +0530

    Update Day 24: More Linked Lists.java

--- Day 24: More Linked Lists.java
+++ Day 24: More Linked Lists.java
@@ -38,3 +38,20 @@ class Solution
        }
        return head;
    }
+	public static  Node insert(Node head,int data)
+    {
+        Node p=new Node(data);			
+        if(head==null)
+            head=p;
+        else if(head.next==null)
+            head.next=p;
+        else
+        {
+            Node start=head;
+            while(start.next!=null)
+                start=start.next;
+            start.next=p;
+
+        }
+        return head;
+    }
