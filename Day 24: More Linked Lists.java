d2c2999a543e704ce213dbbe977cf736aa388ecc
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-06-01 22:53:59 +0530

    Hackerrank solution

--- Day 24: More Linked Lists.java
+++ Day 24: More Linked Lists.java
@@ -64,3 +64,17 @@ class Solution
                   start=start.next;
               }
         }
+	public static void main(String args[])
+        {
+              Scanner sc=new Scanner(System.in);
+              Node head=null;
+              int T=sc.nextInt();
+              while(T-->0){
+                  int ele=sc.nextInt();
+                  head=insert(head,ele);
+              }
+              head=removeDuplicates(head);
+              display(head);
+
+       }
+    }
