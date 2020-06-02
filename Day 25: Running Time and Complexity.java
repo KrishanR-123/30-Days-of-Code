b06d82f901acd7a98e67adee61287f5dc84be4ac
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-06-02 22:48:41 +0530

    Hackerrank solution

--- Day 25: Running Time and Complexity.java
+++ Day 25: Running Time and Complexity.java
@@ -14,3 +14,19 @@ public class Solution {
         }
         return true;
     }
+    
+     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int T = in.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            int n = in.nextInt();
+
+            if (n >= 2 && isPrime(n))
+                System.out.println("Prime");
+            else System.out.println("Not prime");
+        }
+
+        in.close();
+    }
+}
