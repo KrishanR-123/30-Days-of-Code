19c7aedcee257f0f72a431bc597bba9e59b77ae6
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-06-02 22:48:14 +0530

    Update Day 25: Running Time and Complexity.java

--- Day 25: Running Time and Complexity.java
+++ Day 25: Running Time and Complexity.java
@@ -5,3 +5,12 @@ import java.math.*;
 import java.util.regex.*;
 import java.util.Scanner;
 
+public class Solution {
+    public static boolean isPrime(int n) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
