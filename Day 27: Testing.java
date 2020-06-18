d8860a7861f321a49244c3dcd70b28a8cef94ab5
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-06-18 23:25:29 +0530

    Update Day 27: Testing.java

--- Day 27: Testing.java
+++ Day 27: Testing.java
@@ -1,2 +1,16 @@
 import java.util.*;
 
+public class Solution {
+
+    public static int minimum_index(int[] seq) {
+        if (seq.length == 0) {
+            throw new IllegalArgumentException("Cannot get the minimum value index from an empty sequence");
+        }
+        int min_idx = 0;
+        for (int i = 1; i < seq.length; ++i) {
+            if (seq[i] < seq[min_idx]) {
+                min_idx = i;
+            }
+        }
+        return min_idx;
+    }
