3013ad284b4d5612b9f20c4f31e42f9f243cf1c4
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-06-18 23:31:01 +0530

    Update Day 27: Testing.java

--- Day 27: Testing.java
+++ Day 27: Testing.java
@@ -73,3 +73,23 @@ public class Solution {
             throw new AssertionError("result is different than the expected result");
         }
     }
+    
+    public static void TestWithExactlyTwoDifferentMinimums() {
+        int[] seq = TestDataExactlyTwoDifferentMinimums.get_array();
+        if (seq.length < 2) {
+            throw new AssertionError("less than 2 elements in the array");
+        }
+
+        int[] tmp = seq.clone();
+        Arrays.sort(tmp);
+        if (!(tmp[0] == tmp[1] && (tmp.length == 2 || tmp[1] < tmp[2]))) {
+            throw new AssertionError("there are not exactly two minimums in the array");
+        }
+
+        int expected_result = TestDataExactlyTwoDifferentMinimums.get_expected_result();
+        int result = minimum_index(seq);
+        if (result != expected_result) {
+            throw new AssertionError("result is different than the expected result");
+        }
+    }
+
