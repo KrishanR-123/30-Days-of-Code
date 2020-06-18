8badc651aea2b905cbc81a9476b828efc5eb5d4f
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-06-18 23:37:31 +0530

    Update Day 29: Bitwise AND.java

--- Day 29: Bitwise AND.java
+++ Day 29: Bitwise AND.java
@@ -1,1 +1,14 @@
 import java.util.Scanner;
+
+public class Solution {
+
+    public static int bitwiseAnd(int n, int k) {
+        int res = 0;
+
+        for (int i = 1; i < n; i++)
+            for (int j = i + 1; j <= n; j++)
+                if ((i & j) < k)
+                    res = Math.max(res, i & j);
+
+        return res;
+    }
