d57554d2fe5726bfaa00dbfd664cc4216f1d3567
KrishanR-123 <46771395+KrishanR-123@users.noreply.github.com>
2020-06-18 23:38:22 +0530

    Hackerank solution

--- Day 29: Bitwise AND.java
+++ Day 29: Bitwise AND.java
@@ -14,4 +14,24 @@ public class Solution {
     }
     
     private static final Scanner scanner = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            String[] nk = scanner.nextLine().split(" ");
+
+            int n = Integer.parseInt(nk[0]);
+
+            int k = Integer.parseInt(nk[1]);
+
+            System.out.println(bitwiseAnd(n, k));
+        }
+
+        scanner.close();
+    }
+
+}
+
 
